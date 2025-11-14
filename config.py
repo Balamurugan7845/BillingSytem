38b1f014922da6d7d6153c9d7765e213ced62f67
Balamurugan7845 <https://github.com/Balamurugan7845/project-.git>
2025-11-14 14:26:25 +0530

    Change the name

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config:
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'bala1234')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'shop_billing_system')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'shop_billing')
     MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))
